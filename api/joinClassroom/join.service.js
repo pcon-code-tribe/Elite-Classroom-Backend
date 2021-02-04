f79daadd6109e29a4edd0de2d81d159b9e60ba5c
bhavyashukla16 <73889955+bhavyashukla16@users.noreply.github.com>
2021-02-04 11:45:02 +0530

    joinClassroom endpoint improved and getClasses endpoint added

--- api/joinClassroom/join.service.js
+++ api/joinClassroom/join.service.js
@@ -6,9 +6,9 @@ module.exports = {
 
         return new Promise (async (resolve, reject) =>{
 
-            let sql = 'INSERT INTO classes SET user_id = (SELECT user_id FROM users WHERE registration_no = ?), class_code = ?';
+            let sql = 'INSERT INTO classes SET user_id = (SELECT user_id FROM users WHERE registration_no = ?), class_code = ?, class_name = (SELECT class_name FROM classroom WHERE class_code = ?)';
 
-            await pool.query(sql, [reg_id, classCode], 
+            await pool.query(sql, [reg_id, classCode, classCode], 
                 (err, result, field) =>{
 
                 if(err){
--- api/router.js
+++ api/router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/users', require('./users/user.router'));
 router.use('/joinClassroom', require('./joinClassroom/join.router'));
+router.use('/getClasses', require('./getClasses/getClasses.router'));
 
 module.exports = router;
