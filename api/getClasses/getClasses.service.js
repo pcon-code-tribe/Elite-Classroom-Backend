622ef7ee5bf6d7774e042e495cb8a9f8c8a13b1c
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-02-13 01:06:07 +0545

    changed getClasses to use google_token

--- api/getClasses/getClasses.service.js
+++ api/getClasses/getClasses.service.js
@@ -1,12 +1,12 @@
 const pool = require('../../config/database');
 
 module.exports = {
-  getClass: ({ reg_id }) => {
+  getClass: ({ google_token }) => {
     return new Promise(async (resolve, reject) => {
       let sql =
-        'SELECT classroom.class_name, classroom.class_code, classroom.prof_id, users.registration_no FROM classroom, classes, users WHERE users.registration_no = ? AND classes.user_id = users.user_id';
+        'SELECT classroom.class_name, classroom.class_code, classroom.owner_id, (SELECT name FROM users WHERE classroom.owner_id = users.user_id) as owner_name FROM classroom, users, classes WHERE users.google_token = ? AND classes.user_id = users.user_id';
 
-      await pool.query(sql, [reg_id], (err, result, field) => {
+      await pool.query(sql, [google_token], (err, result, field) => {
         if (err) {
           return reject({
             status: 500,
