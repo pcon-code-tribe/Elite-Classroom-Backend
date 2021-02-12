2a28b74ad83871d0cf3e71cdcb8836905d947d6f
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-02-13 00:44:18 +0545

    made classroom to be created from google_token

--- api/classrooms/classroom.service.js
+++ api/classrooms/classroom.service.js
@@ -2,77 +2,98 @@ const { nanoid } = require('nanoid');
 const pool = require('../../config/database');
 
 module.exports = {
-  createClassroom: ({ className, profID }) => {
+  createClassroom: ({ className, google_token }) => {
     return new Promise(async (resolve, reject) => {
       let classCode = nanoid(20);
       let sql =
-        'INSERT INTO classroom(class_code, class_name, prof_id) VALUES (?,?,?)';
+        'INSERT INTO classroom (class_code, class_name, owner_id ) VALUES (?, ?, (SELECT user_id FROM users WHERE google_token = ?))';
 
       await pool.query(
         sql,
-        [classCode, className, profID],
-        (err, result, field) => {
-          if (err) {
+        [classCode, className, google_token],
+        async (error, result, field) => {
+          if (error) {
             return reject({
               status: 500,
-              error: err,
+              error,
             });
           }
 
-          return resolve(result);
+          //  inserting owner into the classroom made
+          let insertOwner =
+            'INSERT INTO classes SET user_id = (SELECT user_id FROM users WHERE google_token = ?), class_code = ?';
+
+          await pool.query(
+            insertOwner,
+            [google_token, classCode],
+            (error, result, field) => {
+              if (error) {
+                return reject({
+                  status: 500,
+                  error,
+                });
+              }
+              return resolve(result);
+            }
+          );
         }
       );
     });
   },
 
-  joinClassroom: ({ reg_id, classCode }) => {
+  joinClassroom: ({ classCode, google_token }) => {
     return new Promise(async (resolve, reject) => {
-      let sqlFindUser = 'SELECT user_id FROM users WHERE registration_no = ?'; //  checking if the user is in the database
+      let sqlFindUser = 'SELECT user_id FROM users WHERE google_token = ?'; //  checking if the user is in the database
 
-      await pool.query(sqlFindUser, [reg_id], async (err, result, field) => {
-        if (result.length === 0) {
-          return reject({
-            status: 500,
-            error: 'User is not registered',
-          });
-        }
-
-        let sqlSearch =
-          'SELECT classes.class_code FROM classes, users WHERE users.registration_no = ? AND users.user_id = classes.user_id AND classes.class_code = ?'; //  checking if user already joined classroom
-
-        await pool.query(
-          sqlSearch,
-          [reg_id, classCode],
-          async (err, result, field) => {
-            if (err) {
-              return reject({
-                status: 500,
-                error: err,
-              });
-            }
-
-            if (result.length === 0) {
-              let sqlInsert =
-                'INSERT INTO classes SET user_id = (SELECT user_id FROM users WHERE registration_no = ?), class_code = ?';
-              await pool.query(
-                sqlInsert,
-                [reg_id, classCode],
-                (err, result, field) => {
-                  if (err) {
-                    return reject({
-                      status: 500,
-                      error: err,
-                    });
-                  }
-                  return resolve(result);
-                }
-              );
-            }
-
-            return resolve(result);
+      await pool.query(
+        sqlFindUser,
+        [google_token],
+        async (error, result, field) => {
+          if (result.length === 0) {
+            return reject({
+              status: 500,
+              error,
+              message: 'User is not registered',
+            });
           }
-        );
-      });
+
+          let sqlSearch =
+            'SELECT classes.class_code FROM classes, users WHERE users.google_token = ? AND users.user_id = classes.user_id AND classes.class_code = ?'; //  checking if user already joined classroom
+
+          await pool.query(
+            sqlSearch,
+            [google_token, classCode],
+            async (error, result, field) => {
+              if (error) {
+                return reject({
+                  status: 500,
+                  error,
+                });
+              }
+
+              if (result.length === 0) {
+                let sqlInsert =
+                  'INSERT INTO classes SET user_id = (SELECT user_id FROM users WHERE google_token = ?), class_code = ?';
+                await pool.query(
+                  sqlInsert,
+                  [google_token, classCode],
+                  (error, result, field) => {
+                    if (error) {
+                      return reject({
+                        status: 500,
+                        error,
+                      });
+                    }
+                    return resolve(result);
+                  }
+                );
+              }
+
+              return resolve(result);
+            }
+          );
+        }
+      );
     });
   },
 };
--- api/router.js
+++ api/router.js
@@ -1,16 +1,13 @@
-const { Route53Resolver } = require('aws-sdk');
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 router.use('/users', require('./users/user.router'));
 router.use('/classrooms', require('./classrooms/classroom.router'));
-router.use('/todo', require('./todos/todo.router'));
-router.use('/classwork', require('./classwork/classwork.router'));
+router.use('/todos', require('./todos/todo.router'));
+router.use('/classworks', require('./classwork/classwork.router'));
 router.use('/getClasses', require('./getClasses/getClasses.router'));
 
-router.use('/notes',require('./notes/notes.router'));
+router.use('/notes', require('./notes/notes.router'));
 
-router.use('/work',require('./work/work.router'));
+router.use('/work', require('./work/work.router'));
 
 module.exports = router;
-
