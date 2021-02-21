b1dd5149172f532312b4024955a88b9a07652c11
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-02-21 23:44:03 +0545

    refactored code

--- chat/chat.js
+++ chat/chat.js
@@ -1,99 +1,83 @@
-const socketIO = require('socket.io');
-const express = require('express');
-const {writeRecord,readRecord} = require('./recorder.v2');
-const {setMsgInfo,getMsgInfo} = require('./reader');
+const { writeRecord, readRecord } = require('./recorder.v2');
+const { setMsgInfo, getMsgInfo } = require('./reader');
 
 //this stores the current room acquired by each connections
-var connections= new Map();
+var connections = new Map();
 var users = new Map();
 
+module.exports = (io) => (socket) => {
+  console.log('new user connected chat');
 
-module.exports = function(io){
-  const route = express.Router();
-  console.log("chat");
-
-
-
-  io.on('connection',(socket)=>{
-    console.log('new user connected chat');
-
-    //sending and storing messages
-    socket.on('sendMsg',(data)=>{
-      const room = data.class_id;
-      console.log(data);
-      writeRecord(data,err=>{
-        if(err){
-          socket.send("error while sending message");
-        }else{
-          io.sockets.in(`${room}`).emit('newMsg',data);
-        }
-      });
+  //sending and storing messages
+  socket.on('sendMsg', (data) => {
+    const room = data.class_id;
+    console.log(data);
+    writeRecord(data, (err) => {
+      if (err) {
+        socket.send('Error while sending message');
+      } else {
+        io.sockets.in(`${room}`).emit('newMsg', data);
+      }
     });
-
-    //joining a connection to a room class_id
-    socket.on('connectRoom',(data)=>{
-      let {room,user_id} = data;
-      // console.log(room);
-
-      //sets socket used to conect against each userid
-      users.set(user_id,socket);
-      // console.log(users);
-
-      //sets user info for each socket
-      connections.set(socket.client.id,{user_id:user_id,room:room});
-      // console.log(connections);
-
-      readRecord(room,(err,info)=>{
-        if(err){
-          // console.log(err);
-          socket.emit('error',err);
-        }else{
-          // console.log(info);
-          socket.emit('allMsg',info);
-        }
-      });
-      socket.join(`${room}`);
-    });
-
-    //when user reads a message
-    socket.on('readMsg',(data)=>{
-      let {id,user_id,user_name} = data;
-
-      // console.log(id);
-
-      setMsgInfo(data,(err)=>{
-        if(err){
-          console.log(err);
-          socket.emit('error','Opps! some error occured')
-        }
-      });
-
-    });
-
-    //when user wants to get read info for a message
-    socket.on('aboutMsgDetail',(data)=>{
-      let {id} = data;
-
-      getMsgInfo(id,(err,info)=>{
-        if(err){
-          console.log(err);
-          socket.emit('error','failed to get details');
-        }else{
-          // console.log(info);
-            socket.emit('sendMsgDetail',(info));
-        }
-      });
-
-    });
-
-    socket.on('disconnect',()=>{
-      connections.delete(socket.client.id);
-      // console.log(connections);
-      console.log("a user disconnected");
-    })
-
   });
 
-  return route;
+  //joining a connection to a room class_id
+  socket.on('connectRoom', (data) => {
+    let { room, user_id } = data;
+    // console.log(room);
 
-}
+    //sets socket used to connect against each user_id
+    users.set(user_id, socket);
+    // console.log(users);
+
+    //sets user info for each socket
+    connections.set(socket.client.id, { user_id: user_id, room: room });
+    // console.log(connections);
+
+    readRecord(room, (err, info) => {
+      if (err) {
+        // console.log(err);
+        socket.emit('error', err);
+      } else {
+        // console.log(info);
+        socket.emit('allMsg', info);
+      }
+    });
+    socket.join(`${room}`);
+  });
+
+  //when user reads a message
+  socket.on('readMsg', (data) => {
+    let { id, user_id, user_name } = data;
+
+    // console.log(id);
+
+    setMsgInfo(data, (err) => {
+      if (err) {
+        console.log(err);
+        socket.emit('error', 'Oops! some error occurred');
+      }
+    });
+  });
+
+  //when user wants to get read info for a message
+  socket.on('aboutMsgDetail', (data) => {
+    let { id } = data;
+
+    getMsgInfo(id, (err, info) => {
+      if (err) {
+        console.log(err);
+        socket.emit('error', 'failed to get details');
+      } else {
+        // console.log(info);
+        socket.emit('sendMsgDetail', info);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    connections.delete(socket.client.id);
+    // console.log(connections);
+    console.log('a user disconnected');
+  });
+};
--- chat/reader.js
+++ chat/reader.js
@@ -1,83 +1,73 @@
 const fs = require('fs');
 
-fs.open('chat/read.json','r',(err,file)=>{
-  if(err){
-    fs.open('chat/read.json','w',(err,file)=>{
-      if(err){
+fs.open('chat/read.json', 'r', (err, file) => {
+  if (err) {
+    fs.open('chat/read.json', 'w', (err, file) => {
+      if (err) {
         console.log(err);
-      }else{
-        console.log("file created and now reading!!");
+      } else {
+        console.log('File created and now reading!!');
       }
     });
-  }else{
-    console.log("reading messages!!");
+  } else {
+    console.log('Reading messages!!');
   }
 });
 
 module.exports = {
   //sets the info for a msg who read it
-  setMsgInfo:(info,callback)=>{
-    let {id,user_id,user_name} =  info;
+  setMsgInfo: (info, callback) => {
+    let { id, user_id, user_name } = info;
     console.log(id);
 
-    fs.readFile('chat/read.json',(err,data)=>{
-
-      if(err){
+    fs.readFile('chat/read.json', (err, data) => {
+      if (err) {
         console.log(err);
         callback(err);
-      }else{
-
-        try{
-
+      } else {
+        try {
           // console.log(data);
 
           var newData = JSON.parse(data);
-          if(newData[`${id}`] === undefined){
+          if (newData[`${id}`] === undefined) {
             newData[`${id}`] = [JSON.stringify(info)];
-          }else{
+          } else {
             var currData = newData[`${id}`];
             currData.push(JSON.stringify(info));
             newData[`${id}`] = currData;
           }
 
-          fs.writeFile('chat/read.json',JSON.stringify(newData),()=>{});
+          fs.writeFile('chat/read.json', JSON.stringify(newData), () => {});
           callback(null);
-        }catch(err){
+        } catch (err) {
           console.log(err);
           callback(err);
         }
-
       }
-
     });
-
   },
   //read the info for a particular msg with a certain id
-  getMsgInfo:(id,callback)=>{
-    fs.readFile('chat/read.json',(err,data)=>{
-      if(err){
+  getMsgInfo: (id, callback) => {
+    fs.readFile('chat/read.json', (err, data) => {
+      if (err) {
         console.log(err);
         callback(err);
-      }else{
-        
-        try{
-
+      } else {
+        try {
           const allData = JSON.parse(data);
           let tosenddata = allData[`${id}`];
           let readyData = [];
 
-          tosenddata.forEach(item=>{
+          tosenddata.forEach((item) => {
             readyData.push(JSON.parse(item));
           });
 
-          callback(null,readyData);
-
-        }catch(err){
+          callback(null, readyData);
+        } catch (err) {
           console.log(err);
           callback(err);
         }
-
       }
     });
-  }
-}
+  },
+};
